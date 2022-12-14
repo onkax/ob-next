22fb1d674840347f72f406a5968fa8f720c88b09
onkax <41636415+onkax@users.noreply.github.com>
2022-12-14 23:40:48 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   async redirects() {
